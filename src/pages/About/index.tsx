aa503a08b774237edd56ef395246663760916ade
leodeutsch <leorocha97@gmail.com>
2025-07-19 08:48:23 -0300

    Fixed exhibitions info

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -36,15 +36,31 @@ export const About: React.FC = () => {
         <S.Section>
           <S.SectionTitle>{t("about.exhibitions")}</S.SectionTitle>
           <S.SectionContent>
-            2010 - ART GALLERY BRAZIL - Publicação em pintura no livro: FLORES, NATUREZA MORTA E PAISAGENS BRASILEIRAS
-            2013 - CAPA da Revista CADERNOS PAGU - UNICAMP. (40), janeiro – junho 2013
-            2018 - Tribunal de Justiça do Estado da Bahia – Salvador/Ba
-            2018 - Assembléia Legislativa do Estado da Bahia
-            2019 - Galeria Sérgio Amorim – Shopping Barra – Salvador/Ba
-            2020 - III CiênciArt - Galeria Sérgio Amorim – Shopping Barra – Salvador/Ba
-            2020 - Arte e Ancestralidade – Atelier Sérgio Amorim – Salvador/Ba
-            2020/2022 – Galeria B. Arte - Shopping Paralela – Salvador -BA
-            2025 - Tribunal de Justiça do Estado da Bahia -Salvador
+            {
+              "2010 - ART GALLERY BRAZIL - Publicação em pintura no livro: FLORES, NATUREZA MORTA E PAISAGENS BRASILEIRAS"
+            }
+            <br />
+            {
+              "2013 - CAPA da Revista CADERNOS PAGU - UNICAMP. (40), janeiro – junho 2013"
+            }
+            <br />
+            {"2018 - Tribunal de Justiça do Estado da Bahia – Salvador/Ba"}
+            <br />
+            {"2018 - Assembléia Legislativa do Estado da Bahia"}
+            <br />
+            {"2019 - Galeria Sérgio Amorim – Shopping Barra – Salvador/Ba"}
+            <br />
+            {
+              "2020 - III CiênciArt - Galeria Sérgio Amorim – Shopping Barra – Salvador/Ba"
+            }
+            <br />
+            {
+              "2020 - Arte e Ancestralidade – Atelier Sérgio Amorim – Salvador/Ba"
+            }
+            <br />
+            {"2020/2022 – Galeria B. Arte - Shopping Paralela – Salvador -BA"}
+            <br />
+            {"2025 - Tribunal de Justiça do Estado da Bahia -Salvador"}
           </S.SectionContent>
         </S.Section>
       </S.ContentSection>
