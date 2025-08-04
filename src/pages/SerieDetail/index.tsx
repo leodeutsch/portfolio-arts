1991275175748d6e5ee01a0deba3550759344520
leodeutsch <leorocha97@gmail.com>
2025-08-04 17:54:26 -0300

    Fixed error after merge

--- src/pages/SerieDetail/index.tsx
+++ src/pages/SerieDetail/index.tsx
@@ -59,65 +59,7 @@ export const SerieDetail: React.FC = () => {
             {getSerieDescription(serie.id, t)}
           </S.SerieDescription>
           <S.ArtworkCount>
-            export const SerieDetail: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate();
-  const { t } = useLanguage();
-  const [serie, setSerie] = useState<Serie | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (!id) return;
-    setError(null);
-    fetchSerieById(id)
-      .then((col) => setSerie(col || null))
-      .catch((err) => {
-        setError(t("common.errorLoadingData"));
-        setSerie(null);
-        console.error(err);
-      })
-      .finally(() => setLoading(false));
-  }, [id, t]);
-
-  const handleBack = () => {
-    navigate("/series");
-  };
-
-  if (loading) {
-    return <S.LoadingContainer>{t("common.loading")}</S.LoadingContainer>;
-  }
-
-  if (error) {
-    return <S.ErrorContainer>{error}</S.ErrorContainer>;
-  }
-
-  if (!serie) {
-    return <S.NotFound>Série não encontrada / Serie not found</S.NotFound>;
-  }
-
-  return (
-    <S.Container>
-      <S.Header>
-        <S.BackButton onClick={handleBack}>
-          <ArrowLeft size={20} />
-          {t("common.back")}
-        </S.BackButton>
-        <S.SerieInfo>
-          <S.SerieTitle>{serie.name}</S.SerieTitle>
-          <S.SerieDescription>
-            {getSerieDescription(serie.id, t)}
-          </S.SerieDescription>
-          <S.ArtworkCount>
             {t("series.artworksCount") + " " + serie?.artworks.length}
-          </S.ArtworkCount>
-        </S.SerieInfo>
-      </S.Header>
-      <ArtworkGrid artworks={serie.artworks} />
-      <Copyright />
-    </S.Container>
-  );
-};
           </S.ArtworkCount>
         </S.SerieInfo>
       </S.Header>
